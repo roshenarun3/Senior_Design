47392ca6a500296e6215bee616622c8b7b8639bd
roshenarun3 <roshenarun3@gmail.com>
2022-11-16 14:29:18 -0500

    Roshen Changes

--- FInal CSVs/James_MotorCombo.m
+++ FInal CSVs/James_MotorCombo.m
@@ -8,19 +8,19 @@ function D = James_MotorCombo(Combo_number)
     i = Combo_number+2; 
     
     % Read data in table
-    data = readmatrix('Acceptable_Motors2.csv'); 
+    data = readmatrix('Acceptable_Motors3.csv');
     
     % Thrust struct for all thrust values of a motor
-    Thrust = [ data(2,12:26) ; data(i,12:26)]';
+    Thrust = [ data(1,12:26) ; data(i,12:26)]';
 
     % Current struct for all current values of a motor
-    Current = [ data(2,27:41) ; data(i,27:41)]';
+    Current = [ data(1,27:41) ; data(i,27:41)]';
 
     % Power struct for all power values of a motor
-    Power = [ data(2,42:56) ; data(i,42:56)]';
+    Power = [ data(1,42:56) ; data(i,42:56)]';
 
     % Rpm  struct for all rpm values of a motor
-    Rpm = [ data(2,57:71) ; data(i,57:71)]';
+    Rpm = [ data(1,57:71) ; data(i,57:71)]';
 
     % Compiles the static characteristics and then puts the structs above
     % into a larger struct so we can treat them all as one combination
@@ -29,6 +29,6 @@ function D = James_MotorCombo(Combo_number)
     D = struct("Motor_Name", data.Var1(i), "Cost", data.Var2(i), "Weight_g", data.Var3(i), "Kv", data.Var4(i),  ...
     "MotorDiam",data.Var5(i),"MotorLen", data.Var6(i), "ShaftDiam", data.Var7(i), "Volt_min", data.Var8(i), "Volt_max", data.Var9(i), "PropD", data.Var10(i),...
     "Prop_Pitch", data.Var11(i), ...
-    "Thrust_g", Thrust, "Current_A",Current, "Power_W", Power, "Rpm", Rpm); 
+    "Thrust_N", Thrust, "Current_A",Current, "Power_W", Power, "Rpm", Rpm); 
 
 end 
