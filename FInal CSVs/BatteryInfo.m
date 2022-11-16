8ff0fe50b0ee9947a12c29d010f4de7959ccc436
JamesDrosdick <jamesdrosdick@vt.edu>
2022-11-15 22:13:09 -0500

    created separate function files from test script

---
+++ FInal CSVs/BatteryInfo.m
@@ -0,0 +1,14 @@
+function D = BatteryInfo(Battery_number)    
+    % Function Desciption
+    % Takes in a number coresponding to a given motor and prop conbination
+    % and returns a struct that contains all data related to Battery
+    % Read data in table
+    data = readtable('Battery Data 2.csv', "Delimiter", ',' ); 
+    
+    % Offset to ignore all the column headers    
+    i = Battery_number; 
+    
+        D = struct("Battery_Name", data.BatteryName(i), "Power_Cap_mAH", data.PowerCapacity_mAh_(i), "Voltage", data.Voltage_V_(i), "Energy_Wh", data.Energy_Wh_(i), "Cont_Discharge_C", data.ContinuousDischargeRate_C_(i), ...
+            "Const_Discharge_A",data.ContinuousDischargeRate_A_(i),"Max_Discharge_C", data.MaximumDischargeRate_c_(i), "Weight_kg", data.Weight_kg_(i), "Dimensions", data.DimensionsMmXMmXMm(i), "Number_Cells_S", data.x_OfCells(i), "Cost", data.Cost___(i)); 
+
+end
