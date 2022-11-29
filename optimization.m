2ea629f0f6c72a329c9dcaf46a977ba84ef70f59
JamesDrosdick <jamesdrosdick@vt.edu>
2022-11-29 18:15:27 -0500

    Update to optimization and new function Mission Sim
    james likes to code!

--- optimization.m
+++ optimization.m
@@ -3,9 +3,10 @@
 clc; clear; close all;
 % initialize
 Passed_configs = zeros(1,4);
-Payload = 25; %lbs
+Payload = 55; %lbs
 count = 1;
 t = 0;
+Distance = 16; %distance in miles
 %% loop
 for i = 1:10
     for j = 1:15
@@ -17,12 +18,13 @@ for i = 1:10
             [CurrentCheck, ThrustCheck, TDF] = Current_thrust_Check(D,B,Payload,k);
             % Tracking 
             if (CurrentCheck == 1 && ThrustCheck == 1)
-                t = t+1;
-                Passed_configs(t,:) = [t,i,j,k];
+                %% Run the mission sim
+                [Mission_Check, Bat_Remainder] = MissionSim(D, B, Payload, Distance, TDF);
+                if (Mission_Check == 1)
+                    t = t+1;
+                    Passed_configs(t,:) = [t,i,j,k];
+                end
             end
-            count = count + 1;
-            % ahere
-            
         end 
     end
 end
