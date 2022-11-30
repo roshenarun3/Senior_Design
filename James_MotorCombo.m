f9e1a33425da588f6cc1969d1bc487dd646ad8df
JamesDrosdick <jamesdrosdick@vt.edu>
2022-11-30 00:54:06 -0500

    Update James_MotorCombo.m

--- James_MotorCombo.m
+++ James_MotorCombo.m
@@ -8,22 +8,22 @@ function D = James_MotorCombo(Combo_number)
     oldfolder = cd('CSVs');
     
     % Offset to ignore all the column headers    
-    i = Combo_number+2; 
+    i = Combo_number+1; 
     
     % Read data in table
-    data = readmatrix('Acceptable_Motors3.csv');
+    data = readmatrix('Acceptable_Motors3.csv','Range','A2:BS12');
     
     % Thrust struct for all thrust values of a motor
-    Thrust = [ data(2,12:26) ; data(i,12:26)]';
+    Thrust = [ data(1,12:26) ; data(i,12:26)]';
 
     % Current struct for all current values of a motor
-    Current = [ data(2,27:41) ; data(i,27:41)]';
+    Current = [ data(1,27:41) ; data(i,27:41)]';
 
     % Power struct for all power values of a motor
-    Power = [ data(2,42:56) ; data(i,42:56)]';
+    Power = [ data(1,42:56) ; data(i,42:56)]';
 
     % Rpm  struct for all rpm values of a motor
-    Rpm = [ data(2,57:71) ; data(i,57:71)]';
+    Rpm = [ data(1,57:71) ; data(i,57:71)]';
 
     % Compiles the static characteristics and then puts the structs above
     % into a larger struct so we can treat them all as one combination
