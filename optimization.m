8c0aca8cc17c73a79fe3cbf4cba9c0a8c3096eaf
JamesDrosdick <jamesdrosdick@vt.edu>
2022-12-01 00:53:35 -0500

    updated optimization

--- optimization.m
+++ optimization.m
@@ -3,10 +3,7 @@
 clc; clear; close all;
 % initialize
 Passed_configs = zeros(1,6);
-Bat_Remainder_percent = [];
 Payload = 25; %lbs
-count = 1;
-best = [];
 t = 0;
 Distance = 16; %distance in miles
 % loop
@@ -20,11 +17,10 @@ for i = 1:10
             % Tracking 
             if (CurrentCheck == 1 && ThrustCheck == 1)
                 % Run the mission sim
-                [Mission_Check, Bat_Remainder] = MissionSim(D, B, Payload, Distance, TDF);
+                [Mission_Check, BCR, BCRP] = MissionSim(D, B, Distance, TDF);
                 if (Mission_Check == 1)
                     t = t+1;
-                    Bat_Remainder_percent = (Bat_Remainder/str2double(B.Battery_Cap_mAH)*100);
-                    Passed_configs(t,:) = [i,j,k, Bat_Remainder, Bat_Remainder_percent, Payload];
+                    Passed_configs(t,:) = [i,j,k, BCR, BCRP, Payload];
                 end
             end
         end 
@@ -32,11 +28,13 @@ for i = 1:10
 end
 %% find best run
 format shortG
-disp(Passed_configs)
+T = array2table(Passed_configs);
+T.Properties.VariableNames(1:6) = {'MP#','B#','# of Bats','BCR mAh','BCRP','Payload lbs'};
+disp(T)
 % find best run
-[BRP,I] = max(Passed_configs(:,5));
+[BCRP,I] = max(Passed_configs(:,5));
 best = Passed_configs(I,:);
-best_percent = num2str(BRP) + "%";
+best_percent = num2str(BCRP) + "%";
 format shortG
 fprintf('Best run configuration is [')
 fprintf('%g, ',best(1:end-1));
